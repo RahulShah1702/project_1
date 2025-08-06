c9a4e34275f92540b1cf28613acfcb177617fe82
RahulShah1702 <rahulshah07022003@gmail.com>
2025-08-07 01:20:10 +0530

    updated server.js file

--- server/server.js
+++ server/server.js
@@ -11,14 +11,17 @@ const port = process.env.PORT || 5000;
 app.use(cors({ origin: 'https://volunteer-web-app.netlify.app' }));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err.message));
 
 app.use('/api/applicants', applicantRoutes);
 app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
